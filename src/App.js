dc16800254ffef69293b3b75a65d3a929487c377
gegham <doctorggk@gmail.com>
2021-06-02 17:34:06 +0400

    redirect

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   }, []);
   return (
     <div className="App" data={data}>
-      <Router>
+      <Router basename="/">
         <SideBar data={data} />
         <Form />
       </Router>
