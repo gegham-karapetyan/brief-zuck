f6c93931587fa680776dab8d6d620fc81ce0bab1
gegham <doctorggk@gmail.com>
2021-07-01 11:38:22 +0400

    t

--- src/pages/StrategyBrief/StrategyBrief.jsx
+++ src/pages/StrategyBrief/StrategyBrief.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { selectBriefAC } from "../../features/selectBriefReducer";
 
-import DayPickerInput from "../../components/DayPickerInput";
-import "react-day-picker/lib/style.css";
-import "./style.scss";
+// import DayPickerInput from "../../components/DayPickerInput";
+// import "react-day-picker/lib/style.css";
+// import "./style.scss";
 
 const StrategyBrief = (props) => {
   const dispatch = useDispatch();
@@ -13,13 +13,13 @@ const StrategyBrief = (props) => {
   });
   return (
     <div>
-      <DayPickerInput
+      {/* <DayPickerInput
         dayPickerProps={{
           month: new Date(2018, 10),
           showWeekNumbers: true,
           todayButton: "Today",
         }}
-      />
+      /> */}
     </div>
   );
 };
